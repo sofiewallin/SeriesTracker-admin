5c392e1c2f40555f35e2cd9b78011814b5f78cbd
Sofie Wallin <sofie.wallin84@gmail.com>
2022-01-24 21:28:45 +0100

    Add Series link in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,6 +17,9 @@ const Header = ({ appName }) => {
             <h2 className='logo'><Link to='/'><img src={Logo} alt={ appName } /></Link></h2>
             <nav id="main-navigation" aria-label='Main navigation'>
                 <ul className="menu">
+                    <li className='menu-item'>
+                        <Link end to='/' className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Series</Link>
+                    </li>
                     <li className='add-series-menu-item'>
                         <Link end to='/add-series' className={({ isActive }) => 'button button-menu' + (isActive ? ' active' : ' inactive')}>Add series</Link>
                     </li>
